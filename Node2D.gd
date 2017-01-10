6713f17ed86fe4f39ea2fd60c3203d9bd38e08d1
m2mbr <m2mbr@localhost>
2017-01-10 17:09:01 -0200

    add script

---
+++ Node2D.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var screen_size
+var pad_size
+var direction = Vector2(1.0, 0.0)
+
+# Constant for pad speed (in pixels/second)
+const INITIAL_BALL_SPEED = 80
+# Speed of the ball (also in pixels/second)
+var ball_speed = INITIAL_BALL_SPEED
+# Constant for pads speed
+const PAD_SPEED = 150
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	pad_size = get_node("left").get_texture().get_size()
+	print(pad_size)
+	set_process(true)
+	print("ready...")
+	
+func _process(delta):
+	pass
