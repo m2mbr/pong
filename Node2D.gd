d1b5ed8714d9d1b930883611c343cc34697baecc
m2mbr <m2mbr@localhost>
2017-01-11 19:53:49 -0200

    change script

--- Node2D.gd
+++ Node2D.gd
@@ -16,7 +16,25 @@ func _ready():
 	pad_size = get_node("left").get_texture().get_size()
 	set_process(true)
 	print("ready...")
-	
+
 func _process(delta):
 	var ball_pos = get_node("ball").get_pos()
+	var left_rect = Rect2( get_node("left").get_pos() - pad_size*0.5, pad_size )
+	var right_rect = Rect2( get_node("right").get_pos() - pad_size*0.5, pad_size )
+	ball_pos += direction * ball_speed * delta
+	# Flip when touching roof or floor
+	if ((ball_pos.y < 0 and direction.y < 0) or (ball_pos.y > screen_size.y and direction.y > 0)):
+	    direction.y = -direction.y
+	# Flip, change direction and increase speed when touching pads
+	if ((left_rect.has_point(ball_pos) and direction.x < 0) or (right_rect.has_point(ball_pos) and direction.x > 0)):
+	    direction.x = -direction.x
+	    direction.y = randf()*2.0 - 1
+	    direction = direction.normalized()
+	    ball_speed *= 1.1
+	# Check gameover
+	if (ball_pos.x < 0 or ball_pos.x > screen_size.x):
+	    ball_pos = screen_size*0.5
+	    ball_speed = INITIAL_BALL_SPEED
+	    direction = Vector2(-1, 0)
+	get_node("ball").set_pos(ball_pos)
 	print(ball_pos)
